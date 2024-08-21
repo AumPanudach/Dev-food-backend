09cfa995b503009ebd0931eb5221833f235889c8
Panudach Kongsomboon <120073076+AumPanudach@users.noreply.github.com>
2024-08-22 04:22:14 +0700

    update router

--- backend/routes/foodRouter.js
+++ backend/routes/foodRouter.js
@@ -4,5 +4,19 @@ import multer from "multer";
 
 const foodRouter = express.Router();
 
+//add image to database
+const storage = multer.diskStorage({
+    destination:"uploads",
+    filename:(req,filename,cb) => {
+        return cb(null, `${Date.now()}${filename.originalname}`)
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
+
+
+foodRouter.post("/add",upload.single('image'),addFood)
 
 export default foodRouter;
